cad168c73d4f8f9b612145f59f0aaeca8c134ab1
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-07-12 20:08:38 +0300

    spring_boot/Conditional приложение

---
+++ src/main/java/ru/netology/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.system.DevProfile;
+import ru.netology.system.ProductionProfile;
+import ru.netology.system.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/system/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.system;
+
+public interface SystemProfile {
+    String getProfile();
+}
